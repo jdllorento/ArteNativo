cf7b2ad768b885d1d9abe5656fe9314d132f18ec
Juan Diego <jdllorento@eafit.edu.co>
2025-03-19 20:09:16 -0500

    Form to add products to database

--- ArteNativo_Project/urls.py
+++ ArteNativo_Project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path('products/', include('products.urls')),
+    path('add_product/', include('admin_panel.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
