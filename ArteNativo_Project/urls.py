e1d9f63b9c3115bab787efcca694149c6728332f
santiagosteam5 <facsonnmartens@gmail.com>
2025-05-15 21:56:39 -0500

    added a redirect from root url to products

--- ArteNativo_Project/urls.py
+++ ArteNativo_Project/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("products/", permanent=False), name="home"),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path('products/', include('products.urls')),
